385ce7bc74b9ca907588426fd3029a1ac60b68ea
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-04-11 11:05:51 -0300

    projeto alura nest-cap2

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+import { Usuario } from './usuario.entity';
+
+@Controller('users')
+export class UsuarioController {
+
+    constructor(private usuarioService: UsuarioService) {}
+
+    @Get(':nomeDeUsuario')
+    public buscaPorNomeDeUsuario(@Param('nomeDeUsuario')nomeDeUsuario: string) {
+        const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario)
+        return usuarioEncontrado;
+    }
+
+    @Post()
+    public cria(@Body() usuario: Usuario): Usuario {
+        const usuarioCriado = this.usuarioService.cria(usuario);
+
+        return usuarioCriado;
+    }
+}
